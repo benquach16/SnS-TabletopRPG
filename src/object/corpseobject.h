2bc285ce370aadbf2a22424c4313b2ed1e771e2b
Ben Quach <benquach16@yahoo.com>
2019-07-22 02:08:13 -0700

    add corpse to git

---
+++ src/object/corpseobject.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "object.h"
+#include "types.h"
+
+class CorpseObject : public Object
+{
+public:
+	CorpseObject(const std::string& name);
+
+	std::string getName() const;
+	std::string getDescription() const override;
+	eObjectTypes getObjectType() const override { return eObjectTypes::Corpse; }
+	void setInventory(const std::map<int, int>& inventory) { m_inventory = inventory; }
+	const std::map<int, int>& getInventory() const { return m_inventory; }
+protected:
+	std::string m_name;
+	eCreatureRace m_race;
+	std::map<int, int> m_inventory;
+};
