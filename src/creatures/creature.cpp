b074c4c751733ee0bae7acfb31175fd0b2b04f92
Ben Quach <benquach16@yahoo.com>
2020-11-04 00:26:22 -0800

    make explusion and other impact manuevers work correctly

--- src/creatures/creature.cpp
+++ src/creatures/creature.cpp
@@ -87,14 +87,19 @@ void Creature::setWeapon(int idx)
 
 void Creature::inflictImpact(int impact)
 {
-    m_currentOffense.dice -= impact;
-    if (m_currentOffense.dice < 0) {
-        int diff = abs(m_currentOffense.dice);
-        m_currentOffense.dice = 0;
-        m_currentPosition.dice -= diff;
-        if (m_currentPosition.dice < 0) {
-            m_combatPool -= -m_currentPosition.dice;
-            m_currentPosition.dice = 0;
+    if (isWeaponDisabled()) {
+        reduceCombatPool(impact);
+    } else {
+        m_currentOffense.dice -= impact;
+
+        if (m_currentOffense.dice < 0) {
+            int diff = abs(m_currentOffense.dice);
+            m_currentOffense.dice = 0;
+            m_currentPosition.dice -= diff;
+            if (m_currentPosition.dice < 0) {
+                m_combatPool -= -m_currentPosition.dice;
+                m_currentPosition.dice = 0;
+            }
         }
     }
 }
